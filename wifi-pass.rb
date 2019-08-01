d8a73ef9d89c242d1f77e42fb773669d4cd732ce
Arthur Factor <46137336+DaFuqtor@users.noreply.github.com>
2019-08-01 22:40:51 +0300

    add head version to wifi-pass.rb

--- wifi-pass.rb
+++ wifi-pass.rb
@@ -2,6 +2,7 @@ class WifiPass < Formula
   desc "Copy, QR encode or just get the password of current Wi-Fi connection"
   homepage "https://wifi-pass.ru"
   url "https://github.com/DaFuqtor/wifi-pass/archive/0.2.7.tar.gz"
+  head "https://github.com/DaFuqtor/wifi-pass.git"
 
   depends_on "qrencode" => :recommended
 
