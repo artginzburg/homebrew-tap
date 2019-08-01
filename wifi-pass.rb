53dc8f90cdb9f848be9abf8ca3cd41cb9e040365
DaFuqtor <46137336+DaFuqtor@users.noreply.github.com>
2019-08-01 19:56:49 +0300

    https homepage

--- wifi-pass.rb
+++ wifi-pass.rb
@@ -1,6 +1,6 @@
 class WifiPass < Formula
   desc "Copy, QR encode or just get the password of current Wi-Fi connection"
-  homepage "http://wifi-pass.ru"
+  homepage "https://wifi-pass.ru"
   url "https://github.com/DaFuqtor/wifi-pass/archive/0.2.6.tar.gz"
   sha256 "907e4e1d02304ab7c1da92aef5b91ae5542cec6da97d3f88e5afd7b977e1fd62"
 
