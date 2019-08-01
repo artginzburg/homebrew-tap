82fbe4126dad880be6377c2317d5468f92db5902
DaFuqtor <46137336+DaFuqtor@users.noreply.github.com>
2019-08-01 21:06:40 +0300

    fix version to enable auto-update

--- wifi-pass.rb
+++ wifi-pass.rb
@@ -2,7 +2,7 @@ class WifiPass < Formula
   desc "Copy, QR encode or just get the password of current Wi-Fi connection"
   homepage "https://wifi-pass.ru"
   url "https://api.github.com/repos/DaFuqtor/wifi-pass/tarball"
-  version ":latest"
+  version "0.2.7"
 
   depends_on "qrencode" => :recommended
 
