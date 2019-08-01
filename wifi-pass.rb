cf8601a15d08e04f0ea1399faa5576de6313eb45
DaFuqtor <46137336+DaFuqtor@users.noreply.github.com>
2019-08-01 20:16:15 +0300

    auto-latest wifi-pass

--- wifi-pass.rb
+++ wifi-pass.rb
@@ -1,8 +1,8 @@
 class WifiPass < Formula
   desc "Copy, QR encode or just get the password of current Wi-Fi connection"
   homepage "https://wifi-pass.ru"
-  url "https://github.com/DaFuqtor/wifi-pass/archive/0.2.6.tar.gz"
-  sha256 "907e4e1d02304ab7c1da92aef5b91ae5542cec6da97d3f88e5afd7b977e1fd62"
+  url "https://api.github.com/repos/DaFuqtor/wifi-pass/tarball"
+  version ":latest"
 
   depends_on "qrencode" => :recommended
 
