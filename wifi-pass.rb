e0818dab9b013784d031135240419bf1bc49cd57
DaFuqtor <46137336+DaFuqtor@users.noreply.github.com>
2019-08-01 21:13:23 +0300

    fix wifi-pass url
    auto-latest is not working as expected, so I have to paste new url for every release

--- wifi-pass.rb
+++ wifi-pass.rb
@@ -1,8 +1,7 @@
 class WifiPass < Formula
   desc "Copy, QR encode or just get the password of current Wi-Fi connection"
   homepage "https://wifi-pass.ru"
-  url "https://api.github.com/repos/DaFuqtor/wifi-pass/tarball"
-  version "0.2.7"
+  url "https://github.com/DaFuqtor/wifi-pass/archive/0.2.7.tar.gz"
 
   depends_on "qrencode" => :recommended
 
