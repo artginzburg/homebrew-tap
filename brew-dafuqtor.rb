e723aaef672de230c34e9c77b15f0fa30c68b5c3
DaFuqtor <46137336+DaFuqtor@users.noreply.github.com>
2019-08-02 14:41:59 +0300

    subdirs

---
+++ brew-dafuqtor.rb
@@ -0,0 +1,8 @@
+require 'formula'
+
+class BrewDafuqtor < Formula
+  homepage 'https://github.com/DaFuqtor/homebrew-DaFuqtor/'
+  url 'https://github.com/DaFuqtor/homebrew-DaFuqtor.git'
+  def install
+  end
+end
