84272c665c497cb3ea20461eddce260f10fc16ec
Arthur Factor <46137336+DaFuqtor@users.noreply.github.com>
2019-08-02 14:45:02 +0300

    Delete brew-dafuqtor.rb

--- brew-dafuqtor.rb
+++
@@ -1,8 +0,0 @@
-require 'formula'
-
-class BrewDafuqtor < Formula
-  homepage 'https://github.com/DaFuqtor/homebrew-DaFuqtor/'
-  url 'https://github.com/DaFuqtor/homebrew-DaFuqtor.git'
-  def install
-  end
-end
