c5507e6136b2623a0466138666572e72c37f4066
DaFuqtor <46137336+DaFuqtor@users.noreply.github.com>
2019-08-02 14:20:08 +0300

    add localswitch.rb
    https://git.io/localswitch

---
+++ localswitch.rb
@@ -0,0 +1,13 @@
+cask 'localswitch' do
+  version '2.2.1'
+  sha256 '32495b3da488ca0e9b55be7ec34df0776165d3c234d8a3444c0c65d815919001'
+
+  url "https://github.com/DaFuqtor/LocalSwitch/releases/download/#{version}/LocalSwitch.zip"
+  appcast 'https://github.com/DaFuqtor/LocalSwitch/releases.atom'
+  name 'LocalSwitch'
+  homepage 'https://git.io/localswitch'
+
+  auto_updates true
+
+  app 'LocalSwitch.app'
+end
