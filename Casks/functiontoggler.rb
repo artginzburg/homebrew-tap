a4d030e468135392537c965b4f7810aa85c79314
DaFuqtor <46137336+DaFuqtor@users.noreply.github.com>
2019-08-07 03:54:10 +0300

    Create functiontoggler.rb

---
+++ Casks/functiontoggler.rb
@@ -0,0 +1,16 @@
+cask 'functiontoggler' do
+  version '1.0'
+  sha256 'fc14965ae4b0c1ebbbc274d6d1d01e81834b08f4b3e5c85885d002ab70205a78'
+
+  url "https://github.com/DaFuqtor/FunctionToggler/releases/download/#{version}/FunctionToggler.zip"
+  appcast 'https://github.com/DaFuqtor/FunctionToggler/releases.atom'
+  name 'Function Toggler'
+  homepage 'https://github.com/DaFuqtor/FunctionToggler'
+
+  auto_updates true
+
+  app 'Function Toggler.app'
+  service 'Function Toggler.workflow'
+
+  zap trash: '~/Library/Caches/com.dafuqtor.FunctionToggler'
+end
