da4eb74b3c39b15c36dfb70db6cd7e27134eab4d
DaFuqtor <46137336+DaFuqtor@users.noreply.github.com>
2019-08-07 18:05:17 +0300

    fix checksum midclick.rb

--- Casks/midclick.rb
+++ Casks/midclick.rb
@@ -1,6 +1,6 @@
 cask 'midclick' do
   version '1.1'
-  sha256 'a83a329493b9140cb73b418ddfa2e921555fc7706a11d8ec2ba13c4016392aca'
+  sha256 '56eee6b0dec04ed4b1789c510d97db486d53035581473ce1926e878ae481e02f'
 
   # github.com was verified as official when first introduced to the cask
   url "https://github.com/DaFuqtor/MiddleClick/releases/download/#{version}/MiddleClick.app.zip"
