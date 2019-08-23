a591dc5c87d80add536b3f063d36efb0b843b7c2
DaFuqtor <46137336+DaFuqtor@users.noreply.github.com>
2019-08-23 11:42:30 +0300

    fix functiontoggler Cask checksum

--- Casks/functiontoggler.rb
+++ Casks/functiontoggler.rb
@@ -1,6 +1,6 @@
 cask 'functiontoggler' do
   version '1.0'
-  sha256 'fc14965ae4b0c1ebbbc274d6d1d01e81834b08f4b3e5c85885d002ab70205a78'
+  sha256 '8c0ebe62b1298d50a791b1bde6bfc6fe3be2bc0716f229f749c50503bca2ca5b'
 
   url "https://github.com/DaFuqtor/FunctionToggler/releases/download/#{version}/FunctionToggler.zip"
   appcast 'https://github.com/DaFuqtor/FunctionToggler/releases.atom'
