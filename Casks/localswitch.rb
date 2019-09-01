a064823647735a46eb5c2bd8443b62b58b83df50
DaFuqtor <46137336+DaFuqtor@users.noreply.github.com>
2019-09-01 13:56:56 +0300

    2.3.2 -> 2.3.3 LocalSwitch Cask

--- Casks/localswitch.rb
+++ Casks/localswitch.rb
@@ -1,6 +1,6 @@
 cask 'localswitch' do
-  version '2.3.2'
-  sha256 'd3563261f94ca6f34669e3a8bb1d760a9a2bb6f2434c9691fd0634cc1611cf84'
+  version '2.3.3'
+  sha256 '5cadf138f3f26cc0e97b055e1899d674124f1bd6eb13ab4ddd8e51415bcd794f'
 
   url "https://github.com/DaFuqtor/LocalSwitch/releases/download/#{version}/LocalSwitch.zip"
   appcast 'https://github.com/DaFuqtor/LocalSwitch/releases.atom'
