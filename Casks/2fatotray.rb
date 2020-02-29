f1e7e9e667522e262d2b54302c52775d9bd8e4ec
DaFuqtor <46137336+DaFuqtor@users.noreply.github.com>
2020-02-29 09:20:38 +0300

    1.4.6 -> 1.4.8.1 2FAtoTray Cask

--- Casks/2fatotray.rb
+++ Casks/2fatotray.rb
@@ -1,6 +1,6 @@
 cask '2fatotray' do
-  version '1.4.6'
-  sha256 '491ed21045cb06939908a308b8c2cd2d7c7d713b8d4a56e2dca54ad88729bba2'
+  version '1.4.8.1'
+  sha256 'cdce86a5774a34f75dc97dfa870fe5e673f232c4c9ec7ddc49a0b5619976150e'
 
   url "https://github.com/DaFuqtor/2FAtoTray/releases/download/#{version}/2FAtoTray.zip"
   appcast 'https://github.com/DaFuqtor/2FAtoTray/releases.atom'
