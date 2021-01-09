0e3eb5cde5002d9e3793b6897d910a258aef88a8
Art Ginzburg <46137336+artginzburg@users.noreply.github.com>
2021-01-09 20:45:26 +0300

    Update username in barhide.rb

--- Formula/barhide.rb
+++ Formula/barhide.rb
@@ -1,8 +1,8 @@
 class Barhide < Formula
   desc "Hide unwanted icons from your menu bar"
-  homepage "https://github.com/DaFuqtor/barhide"
-  url "https://github.com/DaFuqtor/barhide/archive/0.1.tar.gz"
-  head "https://github.com/DaFuqtor/barhide.git"
+  homepage "https://github.com/artginzburg/barhide"
+  url "https://github.com/artginzburg/barhide/archive/0.1.tar.gz"
+  head "https://github.com/artginzburg/barhide.git"
 
   def install
     bin.install "barhide.sh" => "barhide"
