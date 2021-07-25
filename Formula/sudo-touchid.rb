e8461979ed4cb690f8fc1ae6b91ea16e909b6fe0
artginzburg <art.ginzburg@ya.ru>
2021-07-25 15:25:53 +0300

    Create sudo-touchid.rb

---
+++ Formula/sudo-touchid.rb
@@ -0,0 +1,34 @@
+class SudoTouchid < Formula
+  desc "Automate adding TouchID as sufficient sudo auth method"
+  homepage "https://github.com/artginzburg/sudo-touchid"
+  url "https://github.com/artginzburg/sudo-touchid/archive/0.1.tar.gz"
+  head "https://github.com/artginzburg/sudo-touchid.git"
+  sha256 "bbcb0703118a9a19e35e3ec8d7174899324e28a64411c0818d0504d1c9c1e548"
+
+  def install
+    bin.install "sudo-touchid.sh" => "sudo-touchid"
+  end
+
+  plist_options manual: "sudo brew services start sudo-touchid"
+
+  def plist
+    <<~EOS
+      <?xml version="1.0" encoding="UTF-8"?>
+      <!DOCTYPE plist PUBLIC "-//Apple//DTD PLIST 1.0//EN" "http://www.apple.com/DTDs/PropertyList-1.0.dtd">
+      <plist version="1.0">
+        <dict>
+          <key>Label</key>
+          <string>#{plist_name}</string>
+          <key>ProgramArguments</key>
+          <array>
+            <string>#{bin}/sudo-touchid</string>
+          </array>
+          <key>RunAtLoad</key>
+          <true/>
+          <key>KeepAlive</key>
+          <false/>
+        </dict>
+      </plist>
+    EOS
+  end
+end
