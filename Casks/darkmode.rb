ece71aede69acda66b6cd3234aa6e52347628838
Art Ginzburg <ginzburg.arthur@gmail.com>
2021-07-26 23:20:42 +0300

    darkmode: add livecheck & desc, fix username

--- Casks/darkmode.rb
+++ Casks/darkmode.rb
@@ -2,10 +2,10 @@ cask 'darkmode' do
   version '1.0'
   sha256 '8cceb0f17b45438d0209bc9a453e6869300a6272545f0db3419692629e371f9f'
 
-  url "https://github.com/DaFuqtor/DarkMode/releases/download/#{version}/DarkMode.zip"
-  appcast 'https://github.com/DaFuqtor/DarkMode/releases.atom'
+  url "https://github.com/artginzburg/DarkMode/releases/download/#{version}/DarkMode.zip"
   name 'Dark Mode'
-  homepage 'https://github.com/DaFuqtor/DarkMode'
+  desc 'Toggle the macOS dark mode from Spotlight Search'
+  homepage 'https://github.com/artginzburg/DarkMode'
 
   app 'Dark Mode.app'
 end
