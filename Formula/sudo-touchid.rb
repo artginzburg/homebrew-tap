c87b98f535b547d024fdca8a6f1aad4e77726578
artginzburg <art.ginzburg@ya.ru>
2021-07-26 18:40:06 +0300

    add license to sudo-touchid

--- Formula/sudo-touchid.rb
+++ Formula/sudo-touchid.rb
@@ -4,6 +4,7 @@ class SudoTouchid < Formula
   url "https://github.com/artginzburg/sudo-touchid/archive/0.1.tar.gz"
   head "https://github.com/artginzburg/sudo-touchid.git"
   sha256 "bbcb0703118a9a19e35e3ec8d7174899324e28a64411c0818d0504d1c9c1e548"
+  license "EPL-2.0"
 
   def install
     bin.install "sudo-touchid.sh" => "sudo-touchid"
