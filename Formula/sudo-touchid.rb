0844332fae09771e583106c855c26db3de41b83d
Art Ginzburg <ginzburg.arthur@gmail.com>
2021-09-15 13:37:03 +0300

    hotfix: sudo-touchid.rb

--- Formula/sudo-touchid.rb
+++ Formula/sudo-touchid.rb
@@ -1,7 +1,7 @@
 class SudoTouchid < Formula
   desc "Permanent TouchID support for sudo"
   homepage "https://github.com/artginzburg/sudo-touchid"
-  url "https://github.com/artginzburg/sudo-touchid/releases/0.2/download/sudo-touchid.sh"
+  url "https://github.com/artginzburg/sudo-touchid/releases/download/0.2/sudo-touchid.sh"
   head "https://github.com/artginzburg/sudo-touchid.git"
   sha256 "0d140c8d682ac32b7bc3f8df7b336a37b6d308cd0b12932e193129a31dd5c978"
   license "EPL-2.0"
