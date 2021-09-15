5291871a5a5373a9410b6a7b9036e27810ccc34f
Art Ginzburg <ginzburg.arthur@gmail.com>
2021-09-15 12:51:27 +0300

    update sudo-touchid.rb 0.1 > 0.2

--- Formula/sudo-touchid.rb
+++ Formula/sudo-touchid.rb
@@ -1,9 +1,9 @@
 class SudoTouchid < Formula
   desc "Permanent TouchID support for sudo"
   homepage "https://github.com/artginzburg/sudo-touchid"
-  url "https://github.com/artginzburg/sudo-touchid/archive/0.1.tar.gz"
+  url "https://github.com/artginzburg/sudo-touchid/archive/0.2.tar.gz"
   head "https://github.com/artginzburg/sudo-touchid.git"
-  sha256 "bbcb0703118a9a19e35e3ec8d7174899324e28a64411c0818d0504d1c9c1e548"
+  sha256 "fa650ffdf3844c488778e9dc6243f63df84143996cd33b54cd3679a42fce7752"
   license "EPL-2.0"
 
   def install
