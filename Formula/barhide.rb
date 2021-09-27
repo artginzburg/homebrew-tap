c4ac0b19003a2a03eabb99f50f406874bd7e6acd
Art Ginzburg <ginzburg.arthur@gmail.com>
2021-09-27 17:00:07 +0300

    add stats support to barcode.rb

--- Formula/barhide.rb
+++ Formula/barhide.rb
@@ -1,8 +1,10 @@
 class Barhide < Formula
   desc "Hide unwanted icons from your menu bar"
   homepage "https://github.com/artginzburg/barhide"
-  url "https://github.com/artginzburg/barhide/archive/0.1.tar.gz"
+  url "https://github.com/artginzburg/barhide/releases/download/0.1/barhide.sh"
   head "https://github.com/artginzburg/barhide.git"
+  sha256 "4087155ec21adfad4c8828310bff84537024f1854ddf7444d5eb7abec1d8ed2c"
+  license "MIT"
 
   def install
     bin.install "barhide.sh" => "barhide"
