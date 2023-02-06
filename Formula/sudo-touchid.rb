3d9f93bf03e2db58678fb3c47df764f049c0c4e2
Art Ginzburg <ginzburg.arthur@gmail.com>
2023-02-06 14:03:43 +0400

    sudo-touchid.rb: put `head` after everything
    Ran `brew audit --strict ./Formula/sudo-touchid.rb`

--- Formula/sudo-touchid.rb
+++ Formula/sudo-touchid.rb
@@ -2,9 +2,9 @@ class SudoTouchid < Formula
   desc "Permanent TouchID support for sudo"
   homepage "https://github.com/artginzburg/sudo-touchid"
   url "https://github.com/artginzburg/sudo-touchid/releases/download/0.4/sudo-touchid.sh"
-  head "https://github.com/artginzburg/sudo-touchid.git"
   sha256 "5929f7a7edab448142676ced6c579f3577f86d4035399d90cb2a66543f46f888"
   license "EPL-2.0"
+  head "https://github.com/artginzburg/sudo-touchid.git"
 
   def install
     bin.install "sudo-touchid.sh" => "sudo-touchid"
