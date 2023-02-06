b3abb0c43cfa266a26a9709bab52e456d0ca84a4
Art Ginzburg <ginzburg.arthur@gmail.com>
2023-02-06 14:20:21 +0400

    sudo-touchid.rb: Use `branch: "main"` to specify the default branch
    Ran `brew audit --online --strict ./Formula/sudo-touchid.rb`

--- Formula/sudo-touchid.rb
+++ Formula/sudo-touchid.rb
@@ -4,7 +4,7 @@ class SudoTouchid < Formula
   url "https://github.com/artginzburg/sudo-touchid/releases/download/0.4/sudo-touchid.sh"
   sha256 "5929f7a7edab448142676ced6c579f3577f86d4035399d90cb2a66543f46f888"
   license "EPL-2.0"
-  head "https://github.com/artginzburg/sudo-touchid.git"
+  head "https://github.com/artginzburg/sudo-touchid.git", branch: "main"
 
   def install
     bin.install "sudo-touchid.sh" => "sudo-touchid"
