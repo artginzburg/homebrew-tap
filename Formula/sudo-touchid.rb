d447f0c8117128780749b8bd8e9c592ed2f9126d
Art Ginzburg <ginzburg.arthur@gmail.com>
2025-09-18 03:12:25 +0400

    sudo-touchid 0.5

--- Formula/sudo-touchid.rb
+++ Formula/sudo-touchid.rb
@@ -1,8 +1,8 @@
 class SudoTouchid < Formula
   desc "Permanent TouchID support for sudo"
   homepage "https://github.com/artginzburg/sudo-touchid"
-  url "https://github.com/artginzburg/sudo-touchid/releases/download/0.4/sudo-touchid.sh"
-  sha256 "5929f7a7edab448142676ced6c579f3577f86d4035399d90cb2a66543f46f888"
+  url "https://github.com/artginzburg/sudo-touchid/releases/download/0.5/sudo-touchid.sh"
+  sha256 "838fd7f51204885430ec05849962cfb3be32e6436b73f32cc53db80aa29123df"
   license "EPL-2.0"
   head "https://github.com/artginzburg/sudo-touchid.git", branch: "main"
 
