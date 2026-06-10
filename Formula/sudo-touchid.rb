3fec1690289afca5e8266ff622832311bf05c196
Big Lecodeski <22894442+lecodeski@users.noreply.github.com>
2026-06-10 15:25:17 +0200

    Fix deprecation when calling `depends_on :macos` with `depends_on macos:`
    Fix for Homebrew warning: Calling `depends_on :macos` with `depends_on macos:` is deprecated! Use `depends_on :macos` with `depends_on macos:` inside an `on_macos` block instead.

--- Formula/sudo-touchid.rb
+++ Formula/sudo-touchid.rb
@@ -6,11 +6,13 @@ class SudoTouchid < Formula
   license "EPL-2.0"
   head "https://github.com/artginzburg/sudo-touchid.git", branch: "main"
 
-  # Restrict to macOS since TouchID is macOS-specific
-  depends_on :macos
-  # Optional: Specify minimum macOS version if known (e.g., 10.12.2 for TouchID)
-  depends_on macos: :catalina
-
+  on_macos do
+    # Restrict to macOS since TouchID is macOS-specific
+    depends_on :macos
+    # Optional: Specify minimum macOS version if known (e.g., 10.12.2 for TouchID)
+    depends_on macos: :catalina
+  end
+  
   def install
     # Ensure the script is executable and renamed
     bin.install "sudo-touchid.sh" => "sudo-touchid"
