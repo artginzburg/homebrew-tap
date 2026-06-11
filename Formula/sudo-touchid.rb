6b85fa8367626fe12723231674ebfe576c79e7d4
Samuel Badr <samuel.badr@gmail.com>
2026-06-11 10:16:20 +0200

    Fix sudo-touchid macOS dependency DSL

--- Formula/sudo-touchid.rb
+++ Formula/sudo-touchid.rb
@@ -9,7 +9,9 @@ class SudoTouchid < Formula
   # Restrict to macOS since TouchID is macOS-specific
   depends_on :macos
   # Optional: Specify minimum macOS version if known (e.g., 10.12.2 for TouchID)
-  depends_on macos: :catalina
+  on_macos do
+    depends_on macos: :catalina
+  end
 
   def install
     # Ensure the script is executable and renamed
